60c5856771a5d647e3bfbcc21214728f6b91cab4
jaffrantirta <jaffrantirta@icloud.com>
2024-10-02 23:30:08 +0800

    update example permission array

--- config/jaffranlaraveltools.php
+++ config/jaffranlaraveltools.php
@@ -2,8 +2,24 @@
 return [
     'available_permissions' => [
         // [
-        //     'name' => 'create-point', //permission name
-        //     'roles' => ['super-admin', 'user'] // role that are allowed to access said permission
+        //     'name' => 'user.viewAny',
+        //     'roles' => ['super-admin'', 'admin',],
+        // ],
+        // [
+        //     'name' => 'user.view',
+        //     'roles' => ['super-admin'', 'admin',],
+        // ],
+        // [
+        //     'name' => 'user.create',
+        //     'roles' => ['super-admin'', 'admin', 'client'],
+        // ],
+        // [
+        //     'name' => 'user.update',
+        //     'roles' => ['super-admin'', 'admin',],
+        // ],
+        // [
+        //     'name' => 'user.delete',
+        //     'roles' => ['super-admin'', 'admin',],
         // ],
     ],
     'max_login_attempt' => 3,
